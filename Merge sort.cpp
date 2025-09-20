e8ffc0d48206838cf4caf31de2757f3bb6a6c377
LukaSrbionoski <luka.srbinoski@gmail.com>
2025-09-20 19:26:07 +0200

    Add files via upload

---
+++ Merge sort.cpp
@@ -0,0 +1,87 @@
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <algorithm>
+using namespace std;
+using namespace std::chrono;
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+bool isSorted(const vector<int>& arr) {
+    for (size_t i = 1; i < arr.size(); i++) {
+        if (arr[i] < arr[i-1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    const int SIZE = 100000;
+    vector<int> arr(SIZE);
+    srand(time(0));
+
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 1000000 + 1;
+    }
+
+    vector<int> original = arr;
+    
+    auto start = high_resolution_clock::now();
+    mergeSort(arr, 0, arr.size() - 1);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    bool sorted = isSorted(arr);
+    cout << "Merge Sort Time: " << duration.count() << " milliseconds" << endl;
+    cout << "Sorted correctly: " << (sorted ? "Yes" : "No") << endl;
+    
+    return 0;
+}
